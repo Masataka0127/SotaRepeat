7bf5ca9e6c8b286e29bfe8d51986ca3c3b62c055
Masataka Shioya <masataka.shioya.0127@gmail.com>
2019-02-21 18:07:49 +0900

    develop/initブランチへの初コミット

---
+++ src/com/example/sotarepeat/SotaRepeat.java
@@ -0,0 +1,14 @@
+/* SotaRepeat.java
+ * 音声認識結果をTTSでオウム返しするアプリ
+ */
+
+package com.example.sotarepeat;
+
+public class SotaRepeat {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
